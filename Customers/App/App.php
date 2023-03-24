82405e79ad005fdd39adbc81fca41bf0fa3d45b0
EBliujus <ev.bliujus@gmail.com>
2023-03-24 14:12:36 +0200

    commit

---
+++ Customers/App/App.php
@@ -0,0 +1,29 @@
+<?php
+namespace App;
+
+class App {
+
+    public static function process() 
+    {
+        $url = explode('/', $_SERVER['REQUEST_URI']);
+        array_shift($url); 
+
+        return self::router($url);
+
+    }
+
+    private static function router(array $url) 
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method == 'GET' && count($url) == 1 && $url[0] === '') 
+        {
+            return 'JOOO';
+        } 
+        
+        else {
+            return ' noooo JOOOOO';
+        }
+    }
+
+}
